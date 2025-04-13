a4ee9bfb2ab2ff7c9170f429584f79230ddf767b
elp3dr0 <64701822+elp3dr0@users.noreply.github.com>
2025-04-13 08:35:46 +0100

    Further logging and debugging resets

--- src/s4if.py
+++ src/s4if.py
@@ -335,5 +335,7 @@ class Rower(object):
         self._callbacks.remove(cb)
 
     def notify_callbacks(self, event):
+        logger.debug(f"Rower.notify_callbacks: Notifing callbacks of event {event}")
         for cb in self._callbacks:
+            logger.debug(f"Rower.notify_callbacks: Notifying callback {cb} of event {event}")
             cb(event)
