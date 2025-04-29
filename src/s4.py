6af2de66031352fc10102ca8a5e5cacb2da529ed
elp3dr0 <64701822+elp3dr0@users.noreply.github.com>
2025-04-29 10:48:51 +0100

    Clarified names of Class characteristics

--- src/s4.py
+++ src/s4.py
@@ -44,30 +44,9 @@ heartbeat_signal = DigitalOutputDevice(HEARTBEAT_PIN, active_high=True, initial_
 NO_ROWING_PULSE_GAP = 300
 
 IGNORE_LIST = ['graph', 'tank_volume', 'display_sec_dec']
-# Power 
-POWER_AVG_STROKES = 4
 
-
-def s4_heart_beat_task(hrm: HeartRateMonitor):
-    """Simulate continuous ANT+ heart rate signal to transmit to the S4 via 3.5mm jack."""
-    while True:
-        hr = hrm.get_heart_rate()
-
-        if hr > 0:
-            hr_period = 60000 / hr  # Convert BPM to ms
-
-            # Generate a 10ms pulse
-            heartbeat_signal.on()
-            time.sleep(0.01)  # 10ms pulse
-            heartbeat_signal.off()
-
-            # Wait for the rest of the heartbeat interval
-            time.sleep((hr_period - 10) / 1000.0)
-        else:
-            # If no valid HR data, keep the signal off and wait 500ms
-            heartbeat_signal.off()
-            time.sleep(0.5)
-
+# Smooth the displayed power by finding the average max power output over a number of strokes 
+NUM_STROKES_FOR_POWER_AVG = 4
 
 class DataLogger(object):
     def __init__(self, rower_interface):
@@ -79,14 +58,14 @@ class DataLogger(object):
 
         self._wr_lock = threading.RLock()
 
-        self._InstaPowerStroke = None
-        self.maxpowerStroke = None
-        self._StrokeStart = None        # Our _StrokeStart is set to True at when the S4 determines pulley accelleration
+        self._RecentStrokesMaxPower = None
+        self.StrokeMaxPower = None
+        self._DrivePhase = None        # Our _DrivePhase is set to True at when the S4 determines pulley accelleration
                                         # and set to False when S4 detects pulley decelleration. It is therefore True
-                                        # throughout the whole Drive phase of the stroke and False during recover phase. 
+                                        # throughout the whole Drive phase of the stroke and False during recovery phase. 
         self._StrokeTotal = None
         self.Watts = None
-        self.AvgInstaPower = None
+        self.LiveAvgPower = None
         self.Lastcheckforpulse = None
         self.PulseEventTime = None
         self.InstantaneousPace = None
@@ -113,12 +92,12 @@ class DataLogger(object):
         logger.debug("DataLogger._reset_state: Attempting lock")
         with self._wr_lock:
             logger.debug("DataLogger._reset_state: Lock attained, setting values")
-            self._InstaPowerStroke = []
-            self.maxpowerStroke = 0
-            self._StrokeStart = False
+            self._RecentStrokesMaxPower = []
+            self.StrokeMaxPower = 0
+            self._DrivePhase = False
             self._StrokeTotal = 0
             self.Watts = 0
-            self.AvgInstaPower = 0
+            self.LiveAvgPower = 0
             self.Lastcheckforpulse = 0
             self.PulseEventTime = 0
             self.InstantaneousPace = 0
@@ -159,9 +138,9 @@ class DataLogger(object):
             if event['type'] in IGNORE_LIST:
                 return
             if event['type'] == 'stroke_start':
-                self._StrokeStart = True
+                self._DrivePhase = True
             if event['type'] == 'stroke_end':
-                self._StrokeStart = False
+                self._DrivePhase = False
             if event['type'] == 'stroke_rate':
                 self.WRValues.update({'stroke_rate': (event['value']*2)})
             if event['type'] == 'total_strokes':
@@ -180,9 +159,9 @@ class DataLogger(object):
                     self.WRValues.update({'speed':event['value']})
             if event['type'] == 'watts':
                 self.Watts = event['value']
-                self.avgInstaPowercalc(self.Watts)
+                self.update_live_avg_power(self.Watts)
             if event['type'] == 'total_kcal':
-                self.WRValues.update({'total_kcal': (event['value']/1000)})  # in cal now in kcal
+                self.WRValues.update({'total_kcal': ((event['value']+500)/1000)})  # convert calories into kCal (add 500 first to implement arithmetic rounding rather than rounding down)
             if event['type'] == 'total_kcal_h':  # must calclatre it first
                 self.WRValues.update({'total_kcal': 0})
             if event['type'] == 'total_kcal_min':  # must calclatre it first
@@ -223,10 +202,10 @@ class DataLogger(object):
                 self.PaddleTurning = True
             else:
                 self.PaddleTurning = False
-                self._StrokeStart = False
+                self._DrivePhase = False
                 self.PulseEventTime = 0
-                self._InstaPowerStroke = []
-                self.AvgInstaPower = 0
+                self._RecentStrokesMaxPower = []
+                self.LiveAvgPower = 0
                 self.WRValuesStandstill()
 
     def reset_requested(self,event):
@@ -255,19 +234,19 @@ class DataLogger(object):
             self.WRValues_standstill.update({'speed': 0})
             self.WRValues_standstill.update({'watts': 0})
 
-    def avgInstaPowercalc(self,watts):
+    def update_live_avg_power(self,watts):
         with self._wr_lock:
-            if self._StrokeStart:
-                self.maxpowerStroke = max(self.maxpowerStroke, watts)
+            if self._DrivePhase:
+                self.StrokeMaxPower = max(self.StrokeMaxPower, watts)
             else:
-                if self.maxpowerStroke:
-                    self._InstaPowerStroke.append(self.maxpowerStroke)
-                    self.maxpowerStroke = 0
-                while len(self._InstaPowerStroke) > POWER_AVG_STROKES:
-                    self._InstaPowerStroke.pop(0)
-                if len(self._InstaPowerStroke) == POWER_AVG_STROKES:
-                    self.AvgInstaPower = int(sum(self._InstaPowerStroke) / len(self._InstaPowerStroke))
-                    self.WRValues.update({'watts': self.AvgInstaPower})
+                if self.StrokeMaxPower:
+                    self._RecentStrokesMaxPower.append(self.StrokeMaxPower)
+                    self.StrokeMaxPower = 0
+                while len(self._RecentStrokesMaxPower) > NUM_STROKES_FOR_POWER_AVG:
+                    self._RecentStrokesMaxPower.pop(0)
+                if len(self._RecentStrokesMaxPower) == NUM_STROKES_FOR_POWER_AVG:
+                    self.LiveAvgPower = int(sum(self._RecentStrokesMaxPower) / len(self._RecentStrokesMaxPower))
+                    self.WRValues.update({'watts': self.LiveAvgPower})
 
 
     def get_WRValues(self):
@@ -310,6 +289,28 @@ class DataLogger(object):
             logger.debug(f"CueBLEANT got values to append to dqueues from S4: {values}")
             ble_out_q.append(values)
             ant_out_q.append(values)
+
+
+def s4_heart_beat_task(hrm: HeartRateMonitor):
+    """Simulate continuous ANT+ heart rate signal to transmit to the S4 via 3.5mm jack."""
+    while True:
+        hr = hrm.get_heart_rate()
+
+        if hr > 0:
+            hr_period = 60000 / hr  # Convert BPM to ms
+
+            # Generate a 10ms pulse
+            heartbeat_signal.on()
+            time.sleep(0.01)  # 10ms pulse
+            heartbeat_signal.off()
+
+            # Wait for the rest of the heartbeat interval
+            time.sleep((hr_period - 10) / 1000.0)
+        else:
+            # If no valid HR data, keep the signal off and wait 500ms
+            heartbeat_signal.off()
+            time.sleep(0.5)
+
 
 def s4_data_task(in_q, ble_out_q, ant_out_q, hrm: HeartRateMonitor):
     logger.debug("s4_data_task: Initialising Rower class")
